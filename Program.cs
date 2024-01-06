1e189ff2cbae10c0c83c725d660dac4232a23c31
Charlotte <charlotteleyser@icloud.com>
2024-01-06 12:25:55 +0100

    Added comments

--- Program.cs
+++ Program.cs
@@ -11,17 +11,27 @@ namespace Mini_project_API
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+            
+            //Create the connection to the database for the API
             string connectionString = builder.Configuration.GetConnectionString("PersonInterestContext");
             builder.Services.AddDbContext<PersonInterestContext>(opt => opt.UseSqlServer(connectionString));
             var app = builder.Build();
 
-            app.MapGet("/", () => "Welcome to persons and interests server!");
+            app.MapGet("/", () => "Welcome to the persons and interests server!");
 
+            //Get all persons in system
             app.MapGet("/persons", PersonHandler.GetAllPersons);
+
+            //Get all links connected to a specific person
             app.MapGet("/persons/{personId}/links", LinkHandler.GetAllPersonLinks);
+
+            //Create new link connected to specfic person and specific interest with a POST request
+            app.MapPost("/persons/{personId}/{interestId}/links", LinkHandler.AddPersonInterestLink);
+
+            //Get all links connected to a specific person
             app.MapGet("/persons/{personId}/interests", InterestHandler.GetPersonAllInterests);
 
-            app.MapPost("/persons/{personId}/{interestId}/links", LinkHandler.AddPersonInterestLink);
+            //Create new interest connected to a specfic person with a POST request
             app.MapPost("/persons/{personId}/interests", InterestHandler.AddNewPersonInterest);
 
             app.Run();
